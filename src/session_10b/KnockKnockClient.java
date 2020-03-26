c6e989560a534f045f2d0146de300dc0e8ebab13
yxd3549 <yxd3549@rit.edu>
2020-03-25 20:23:17 -0400

    annotate client and server with step-by-step instructions

--- src/session_10b/KnockKnockClient.java
+++ src/session_10b/KnockKnockClient.java
@@ -1,6 +1,7 @@
 package session_10b;
 
 import java.io.IOException;
+import java.util.Scanner;
 
 public class KnockKnockClient {
 
@@ -14,6 +15,7 @@ public class KnockKnockClient {
      */
     public KnockKnockClient(String host, int port) throws IOException {
         // TODO #2 Initialize the client and receive the greeting message. Print it out
+
     }
 
     /**
@@ -21,13 +23,75 @@ public class KnockKnockClient {
      * or the server ends communication
      */
     public void hearJokes(){
-        // TODO #3 while you keep getting messages, print them out and prompt the user when necessary
-        //  Most of the interaction with the server occurs here
+        // TODO #3 Complete this method
+        Scanner userIn = new Scanner(System.in); // Use this to get input from the user
+        while(true){
+            String userAnswer = "";
+            // Get an answer to the server's greeting from the user
+
+            if(userAnswer.contains("y")){ // If the user's answer contains the letter y:
+                // Send the YES message to the server
+
+                // Get the knock knock question from the server. Print it out
+
+                // Get a response from the user. The user should ask "Who's there?"
+                // But they may not, the server will handle that.
+                // Send the user's response to the server
+
+                // Get an answer from the server
+                String serverAnswer = "";
+                while(serverAnswer.equals(KnockKnockProtocol.WRONG_QUESTION)){ // While the server isn't satisfied:
+                    // Print out the server's answer
+
+                    // Get a new answer from the user
+
+                    // Send the answer to the server
+
+                    // Get a new response from the server
+                }
+
+                // Print the server's new answer. This will be the clue to the joke
+
+
+                // Get the user's response. They user ask "___ who?"
+                // But they may not, the server will handle that.
+                // Send the user's response to the server
+
+                // Get an answer from the server
+
+                while(serverAnswer.equals(KnockKnockProtocol.WRONG_QUESTION2)){ // while the server isn't satisfied:
+                    // Print out the server's answer
+
+                    // Get a new answer from the user
+
+                    // Send the answer to the server
+
+                    // Get a new response from the server
+                }
+
+                // Print the server's new answer. This is the punchline. Finally!
+
+                // Get a new response form the server. This will be the server's prompt for another joke. Print it
+            } else {
+                // Send the NO message to the server
+
+                // Get a response form the server
+
+                // exit the loop
+            }
+        }
+    }
+
+    /**
+     * Called at the end to close down the network connection.
+     */
+    public void shutDown() throws IOException {
+        // TODO #5 Don't forget to clean up after yourself!
     }
 
 
-    public static void main(String[] args) {
-        // TODO #3 Make an instance of a KnockKnockClient and call hearJokes()
+    public static void main(String[] args) throws IOException{
+        // TODO #4 Make an instance of a KnockKnockClient and call hearJokes()
         //  Use port 1000
         //  Call shutDown() to close all resources.
     }
--- src/session_10b/KnockKnockServer.java
+++ src/session_10b/KnockKnockServer.java
@@ -2,12 +2,13 @@ package session_10b;
 
 import java.io.IOException;
 
+
 public class KnockKnockServer {
 
     /* TODO #1 Add the variables you think you'll need. Look at GuessServer for reference */
 
-    private String[] clues = { "Turnip", "Little Old Lady", "Atch", "Who", "Who" };
-    private String[] answers = { "Turnip the heat, it's cold in here!",
+    private String[] clues = { "Turnip", "LittleOldLady", "Atch", "Who", "Who" };
+    private String[] jokes = { "Turnip the heat, it's cold in here!",
             "I didn't know you could yodel!",
             "Bless you!",
             "Is there an owl in here?",
@@ -24,6 +25,7 @@ public class KnockKnockServer {
     public KnockKnockServer(int port) throws IOException {
         // TODO #2 Initialize the server and send greeting message to client
 
+
     }
 
     /**
@@ -31,8 +33,46 @@ public class KnockKnockServer {
      * the client decides to quit or until it has no more jokes.
      */
     public void tellJokes(){
-        // TODO #4 Iterate through the jokes until the client quits or your out of jokes
-        //  Most of the communication with the client will occur here
+        // TODO #3 Complete this method
+        for(int i = 0; i < clues.length; i++){
+            String clue = this.clues[i];
+            String joke = this.jokes[i];
+
+            // Get a response from the client
+            String answer = "";
+            if (answer.equals(KnockKnockProtocol.YES)){ // If the client wants a joke:
+                // Send the KnockKnock message to the client
+
+                // Get an answer from the Client
+                while(!answer.equals(KnockKnockProtocol.WHOS_THERE)){ // While the answer is not exactly "Who's there?":
+                    // Send the Wrong Question message to the client
+
+                    // Get a new answer from the client
+                }
+
+                // Once you finally get the right response, send the client the clue to the joke
+
+                // Get a response from the client
+
+                String[] pieces = answer.split(" ");
+                while(pieces.length != 2 || !(pieces[0].equals(clue) && pieces[1].equals("who?"))){ // While the answer isn't right:
+                    // Send the Wrong Question 2 message to the client
+
+                    // get a new answer from the client
+
+                    pieces = answer.split(" ");
+                }
+
+                // Finally, send the client the punchline
+
+                // Send the client the Again message
+
+            } else { // if the client does not want a joke:
+                //Send the client the Ending message
+
+                break;
+            }
+        }
     }
 
     /**
@@ -41,12 +81,14 @@ public class KnockKnockServer {
      */
     public void shutDown() throws IOException{
         // TODO #5 Don't forget to clean up after yourself!
+
     }
 
-    public static void main(String[] args) {
-        // TODO #3 Make an instance of a KnockKnockServer and call tellJokes()
+    public static void main(String[] args) throws IOException{
+        // TODO #4 Make an instance of a KnockKnockServer and call tellJokes()
         //  Use port 1000
         //  Call shutDown() to close all resources.
+
     }
 
 
