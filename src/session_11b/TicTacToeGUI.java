bd94b7ff9eb0f7555b369504ca1d4a0b371d77c8
yxd3549 <yxd3549@rit.edu>
2020-04-15 10:35:17 -0400

    complete GUI activity

--- src/session_11b/TicTacToeGUI.java
+++ src/session_11b/TicTacToeGUI.java
@@ -26,6 +26,8 @@ public class TicTacToeGUI extends Application implements Observer<TicTacToeModel
     @Override
     public void init() {
         // TODO: create the model and add ourselves as an observer
+        model=new TicTacToeModel();
+        model.addObserver(this);
     }
 
     @Override
@@ -33,16 +35,16 @@ public class TicTacToeGUI extends Application implements Observer<TicTacToeModel
 
         // TODO: Initialize the Label.
         //  It should display "_'s turn", where _ is the current mark in the model
-
+        text=new Label("X's turn");
 
         // TODO: Initialize the gridPane
-
+        gridPane=new GridPane();
         // Create the buttons
         for(int row = 0; row < 3; row++){
             for (int col = 0; col < 3; col++){
                 // TODO: Create a Button
                 //  - Set the button's text to "-".
-
+                Button button = new Button("-");
 
                 // For reasons that won't make sense until you learn more about programming languages and lambdas,
                 // you need these "final" variables to use inside the button's setOnAction
@@ -51,25 +53,28 @@ public class TicTacToeGUI extends Application implements Observer<TicTacToeModel
 
                 // TODO: Make it so that, when we click a button, we place a mark in the model at the correct location.
                 //  Remember the board is stored in a Row then Column fashion
-
+                button.setOnAction(actionEvent -> model.placeMark(finalRow, finalCol));
 
                 // TODO: Add the button to the GridPane
                 //  remeber the grid stores values in a x then y fashion
+                gridPane.add(button, finalCol, finalRow);
             }
         }
 
         // TODO: Create a button to reset the board
         //  Make it so that when you click on it, the model restarts the board
-
-
+        Button button = new Button("Reset");
+        button.setOnAction(actionEvent -> model.restart());
         // TODO: Create a container to put all your nodes in
         //  Add all the nodes to this container
-
+        VBox vbox = new VBox();
+        vbox.getChildren().addAll(text, gridPane, button);
 
         // TODO: Create a new Scene
         //  Set the stage's scene to the new scene
         //  Show the stage
-        Scene scene = new Scene(new VBox());
+
+        Scene scene = new Scene(vbox);
         stage.setScene(scene);
         stage.show();
     }
@@ -78,9 +83,14 @@ public class TicTacToeGUI extends Application implements Observer<TicTacToeModel
     public void update(TicTacToeModel ticTacToeModel) {
 
         // TODO: Update the label to reflect the new turn
-        this.text.setText(this.model.getTurn() + "'s turn");
-
+        text.setText(model.getTurn() + "'s turn");
         // TODO: Update the gridPane to reflect the changes to the board
+        for(int row = 0; row < 3; row++) {
+            for (int col = 0; col < 3; col++) {
+                Button button = getNodeFromGridPane(gridPane, col, row);
+                button.setText(Character.toString(model.getCharacter(row, col)));
+            }
+        }
     }
 
     /**
