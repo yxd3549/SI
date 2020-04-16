351859a807478ff3d4f427642b86b016701ac4aa
Heather Moses <hlm8500@rit.edu>
2020-04-16 19:51:46 -0400

    Did isSuccess and isGoal

---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+module SI {
+    requires transitive javafx.controls;
+    exports session_11b;
+    exports session_8a;
+    exports session_8b;
+    exports session_10a;
+    exports session_10b;
+    exports session_11a;
+}
